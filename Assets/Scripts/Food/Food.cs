d5fd6d36d67910794c773f7bbb7b05b19bcdbe33
CodySemik <semik.silakovik@gmail.com>
2024-03-13 18:30:55 +0300

    Fixed food spawning
    everything is synchonised

--- Assets/Scripts/Food/Food.cs
+++ Assets/Scripts/Food/Food.cs
@@ -1,19 +1,34 @@
-﻿using System.Collections;
+﻿using Mirror;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Food : MonoBehaviour
+public class Food : NetworkBehaviour
 {
     [SerializeField] GameObject particlePrefab;
-
+    GameObject boom = null;
+    public static event Action OnEat;
     void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
+
         FindObjectOfType<Snake>().AddTail();
-        GameObject boom = Instantiate
-            (particlePrefab, transform.position, particlePrefab.transform.rotation);
-        Destroy(boom, 3f);
-        Destroy(gameObject);
-        FindObjectOfType<FoodSpawner>().SpawnFood();
+
+        ServerParticles();
+        OnEat?.Invoke();
+        NetworkServer.Destroy(gameObject);
+    }
+    IEnumerator DestroyBoom(float time)
+    {
+        yield return new WaitForSeconds(time);
+        NetworkServer.Destroy(boom);
+    }
+    [ServerCallback]
+    void ServerParticles()
+    {
+        boom = Instantiate(particlePrefab, transform.position, particlePrefab.transform.rotation);
+        NetworkServer.Spawn(boom);
+        StartCoroutine(DestroyBoom(1f));
     }
 }
